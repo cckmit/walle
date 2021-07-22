bfbb0a2e3c9ecad34dec16a70ee2ca910c4d992c
lipan <lipan.me@qq.com>
2021-07-22 22:34:12 +0800

    更新

---
+++ walle-study/src/test/java/com/panpan/walle/study/jvm/OOMTest.java
@@ -0,0 +1,14 @@
+package com.panpan.walle.study.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OOMTest {
+    public static void main(String[] args) {
+        List<byte[]> list = new ArrayList<>();
+        while (true){
+            list.add(new byte[100]);
+        }
+
+    }
+}
