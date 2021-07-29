8c4d84467a8f1f54b5dd50c4325eb924987f4b52
lipan <lipan.me@qq.com>
2021-07-29 22:59:24 +0800

    leetcode

---
+++ walle-study/src/main/java/com/panpan/walle/study/alg/leetcode/CoinChangeSolution.java
@@ -0,0 +1,13 @@
+package com.panpan.walle.study.alg.leetcode;
+
+/**
+ * 322:零钱兑换
+ *
+ * 问题描述
+ * https://leetcode-cn.com/problems/coin-change/
+ */
+public class CoinChangeSolution {
+    public int coinChange(int[] coins, int amount) {
+        return 0;
+    }
+}
---
+++ walle-study/src/main/java/com/panpan/walle/study/alg/leetcode/NumSquaresSolution.java
@@ -0,0 +1,15 @@
+package com.panpan.walle.study.alg.leetcode;
+
+/**
+ * 279: 完全平方数
+ * https://leetcode-cn.com/problems/perfect-squares/
+ *
+ * 算法描述
+ * https://leetcode-cn.com/problems/perfect-squares/solution/hua-jie-suan-fa-279-wan-quan-ping-fang-shu-by-guan/
+ *
+ */
+public class NumSquaresSolution {
+    public int numSquares(int n) {
+        return 0;
+    }
+}
