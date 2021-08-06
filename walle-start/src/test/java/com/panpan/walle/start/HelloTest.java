163672667d89c14014fd2d6b9c2811836c75d642
lipan <lipan.me@qq.com>
2021-08-06 16:54:29 +0800

    增加redis功能

--- walle-start/src/test/java/com/panpan/walle/start/HelloTest.java
+++ walle-start/src/test/java/com/panpan/walle/start/HelloTest.java
@@ -2,17 +2,36 @@ package com.panpan.walle.start;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = WalleStartApplication.class)
-@Profile({"dev"})
+@Profile({"lp"})
 public class HelloTest {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Test
     public void test(){
         System.out.println(111);
+    }
+
+
+    @Test
+    public void testRedis(){
+        redisTemplate.opsForValue().set("a","1");
+        redisTemplate.opsForValue().set("b","1");
+        redisTemplate.opsForValue().set("c","1");
+        redisTemplate.opsForValue().set("d","1");
+
+        redisTemplate.opsForValue().get("a");
+        redisTemplate.opsForValue().get("b");
+        redisTemplate.opsForValue().get("c");
+        redisTemplate.opsForValue().get("d");
     }
 }
