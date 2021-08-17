d4805866b3766a90585b19dd51f3b72147459901
lipan <lipan.me@qq.com>
2021-08-17 09:09:43 +0800

    算法

---
+++ walle-study/src/main/java/com/panpan/walle/study/alg/zcy/study/class02/Code02_DeleteGivenValue.java
@@ -0,0 +1,24 @@
+package com.panpan.walle.study.alg.zcy.study.class02;
+
+public class Code02_DeleteGivenValue {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 从链表删除指定值
+     *
+     * @param head
+     * @param num
+     * @return
+     */
+    public static Node removeValue(Node head, int num) {
+
+        return null;
+    }
+}
