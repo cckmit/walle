6e71b9ba1979c17f864ccf8d423f6e2cd21532cd
lipan <lipan.me@qq.com>
2021-11-03 15:51:34 +0800

    leetcode

---
+++ walle-study/src/test/java/com/panpan/walle/study/alg/leetcode/No0338_CountBitsTest.java
@@ -0,0 +1,15 @@
+package com.panpan.walle.study.alg.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class No0338_CountBitsTest {
+
+    @Test
+    public void test(){
+        No0338_CountBits solution = new No0338_CountBits();
+        int[] res = solution.countBits(5);
+    }
+
+}
