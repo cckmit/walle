eb4de7ac3e8c9a0a58e7604d7f31b35da7a772e9
lipan <lipan.me@qq.com>
2022-02-09 23:47:27 +0800

    up

--- walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class01/Problem02_MinWindowLength.java
+++ walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class01/Problem02_MinWindowLength.java
@@ -1,5 +1,14 @@
 package com.zaqbest.walle.study.alg.zcy.advance.zdemo.term05.class01;
 
+/**
+ * 给定字符串str1和str2，求str1的子串中含有str2所有字符的最小子串长度
+ * 【举例】
+ * str1="abcde"，str2="ac"
+ * 因为"abc"包含 str2 所有的字符，并且在满足这一条件的str1的所有子串中，"abc"是 最短的，返回3。
+ * str1="12345"，str2="344" 最小包含子串不存在，返回0。
+ *
+ * {@link com.zaqbest.walle.study.alg.leetcode.Problem_0076_MinWindow}
+ */
 public class Problem02_MinWindowLength {
 
 	public static int minLength(String s1, String s2) {
