f23985903181989ddf99bdf674b72b1c1a3d8ebb
lipan <lipan.me@qq.com>
2022-02-22 17:57:37 +0800

    抛棋子问题

--- walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class04/Code02_ThrowChessPiecesProblem.java
+++ walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class04/Code02_ThrowChessPiecesProblem.java
@@ -1,5 +1,33 @@
 package com.zaqbest.walle.study.alg.zcy.advance.zdemo.term05.class04;
 
+/**
+ * 一座大楼有 0~N 层，地面算作第 0 层，最高的一层为第 N 层。已知棋子从第 0 层掉落肯定 不会摔碎，从第 i 层掉落可能会摔碎，也可能不会摔碎(1≤i≤N)。
+ * 给定整数 N 作为楼层数， 再给定整数 K 作为棋子数，返 回如果想找到棋子不会摔碎的最高层数，即使在最差的情况下扔 的最少次数。一次只能扔一个棋子。
+ * 【举例】
+ * N=10，K=1。
+ * 返回 10。因为只有 1 棵棋子，所以不得不从第 1 层开始一直试到第 10 层，在最差的情况 下，即第 10 层 是不会摔坏的最高层，最少也要扔 10 次。
+ *
+ * N=3，K=2。
+ * 返回 2。先在 2 层扔 1 棵棋子，如果碎了，试第 1 层，如果没碎，试第 3 层。
+ *
+ * N=105，K=2
+ * 返回 14。
+ * 第一个棋子先在 14 层扔，
+ * 碎了则用仅存的一个棋子试 1~13。
+ * 若没碎，第一个棋子继续在 27 层扔，碎了则 用仅存的一个棋子试 15~26。
+ * 若没碎，第一个棋子继续在 39 层扔，碎了则用仅存的一个棋子试 28~38。
+ * 若 没碎，第一个棋子继续在 50 层扔，碎了则用仅存的一个棋子试 40~49。
+ * 若没碎，第一个棋子继续在 60 层扔， 碎了则用仅存的一个棋子试 51~59。
+ * 若没碎，第一个棋子继续在 69 层扔，碎了则用仅存的一个棋子试 61~68。
+ * 若没碎，第一个棋子继续在 77 层扔，碎了则用仅存的一个棋子试 70~76。
+ * 若没碎，第一个棋子继续在 84 层 扔，碎了则用仅存的一个棋子试 78~83。
+ * 若没碎，第一个棋子继续在 90 层扔，碎了则用仅存的一个棋子试 85~89。
+ * 若没碎，第一个棋子继续在 95 层扔，碎了则用仅存的一个棋子试 91~94。
+ * 若没碎，第一个棋子继续 在 99 层扔，碎了则用仅存的一个棋子试 96~98。
+ * 若没碎，第一个棋子继续在 102 层扔，碎了则用仅存的一 个棋子试 100、101。
+ * 若没碎，第一个棋子继续在 104 层扔，碎了则用仅存的一个棋子试 103。
+ * 若没碎，第 一个棋子继续在 105 层扔，若到这一步还没碎，那么 105 便是结果。
+ */
 public class Code02_ThrowChessPiecesProblem {
 
 	public static int solution1(int nLevel, int kChess) {
@@ -22,8 +50,10 @@ public class Code02_ThrowChessPiecesProblem {
 		}
 		int min = Integer.MAX_VALUE;
 		for (int i = 1; i != rest + 1; i++) { // 第一次扔的时候，仍在了i层
+			//最差情况，取个最好值
 			min = Math.min(min,
-					Math.max(Process1(i - 1, k - 1), Process1(rest - i, k)));
+					Math.max(Process1(i - 1, k - 1), //摔坏了
+							Process1(rest - i, k))); //没摔坏
 		}
 		return min + 1;
 	}
@@ -36,11 +66,15 @@ public class Code02_ThrowChessPiecesProblem {
 			return nLevel;
 		}
 		int[][] dp = new int[nLevel + 1][kChess + 1];
+
+		//只有一个棋子的情况
 		for (int i = 1; i != dp.length; i++) {
 			dp[i][1] = i;
 		}
+
+		//考虑大于一个棋子的情况
 		for (int i = 1; i != dp.length; i++) {
-			for (int j = 2; j != dp[0].length; j++) {
+			for (int j = 2; j != dp[0].length; j++) { //棋子数
 				int min = Integer.MAX_VALUE;
 				for (int k = 1; k != i + 1; k++) {
 					min = Math.min(min, Math.max(dp[k - 1][j - 1], dp[i - k][j]));
@@ -146,6 +180,10 @@ public class Code02_ThrowChessPiecesProblem {
 	}
 
 	public static void main(String[] args) {
+
+		solution2(100,2);
+		System.exit(0);
+
 		System.out.println(solution1(21, 2));
 		System.out.println(solution2(21, 2));
 		System.out.println(solution3(21, 2));
