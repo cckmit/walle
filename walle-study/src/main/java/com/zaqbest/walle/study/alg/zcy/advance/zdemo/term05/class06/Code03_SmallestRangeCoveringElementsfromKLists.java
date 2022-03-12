10926b891bf19ff47f3854daf753607ddb167fff
lipan <lipan.me@qq.com>
2022-03-12 09:41:36 +0800

    up

--- walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class06/Code03_SmallestRangeCoveringElementsfromKLists.java
+++ walle-study/src/main/java/com/zaqbest/walle/study/alg/zcy/advance/zdemo/term05/class06/Code03_SmallestRangeCoveringElementsfromKLists.java
@@ -4,6 +4,30 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
+/**
+ * 你有k个 非递减排列 的整数列表。找到一个 最小 区间，使得k个列表中的每个列表至少有一个数包含在其中。
+ *
+ * 我们定义如果b-a < d-c或者在b-a == d-c时a < c，则区间 [a,b] 比 [c,d] 小。
+ *
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
+ * 输出：[20,24]
+ * 解释：
+ * 列表 1：[4, 10, 15, 24, 26]，24 在区间 [20,24] 中。
+ * 列表 2：[0, 9, 12, 20]，20 在区间 [20,24] 中。
+ * 列表 3：[5, 18, 22, 30]，22 在区间 [20,24] 中。
+ * 示例 2：
+ *
+ * 输入：nums = [[1,2,3],[1,2,3],[1,2,3]]
+ * 输出：[1,1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/smallest-range-covering-elements-from-k-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Code03_SmallestRangeCoveringElementsfromKLists {
 
 	public static class Node {
