f1404c5df65d4178646937a9313776a5510efb42
lipan <lipan.me@qq.com>
2022-03-14 19:08:53 +0800

    up

--- walle-study/src/main/java/com/zaqbest/walle/study/misc/domain/TencentDomain.java
+++ walle-study/src/main/java/com/zaqbest/walle/study/misc/domain/TencentDomain.java
@@ -41,7 +41,7 @@ public class TencentDomain {
         try{
             // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
             // 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
-            Credential cred = new Credential("AKIDzpJWifHxOZpoHnjWhGsEqCHH1VpvbTu5", "ECuWnohJeYN2dqngIgofFgwxpRn50fTu");
+            Credential cred = new Credential("tencentSecretId", "tencentSecretKey");
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
             HttpProfile httpProfile = new HttpProfile();
             httpProfile.setEndpoint("domain.tencentcloudapi.com");
